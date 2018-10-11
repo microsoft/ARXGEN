d3d447d7029e5ec49b4a3313fcf5ea2884a0c004
Krishna Sangeeth <kskrishnasangeeth@gmail.com>
2018-10-11 17:04:22 +0530

    Add setup.py for push to PyPi

---
+++ arxiv/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='arxgen',
+    version='0.0.1',
+    packages=['arxgen'],
+    description='ARXGEN ',
+    url = 'https://github.com/microsoft/ARXGEN',
+    package_data={},
+    license='MIT'
+)
